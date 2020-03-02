795d8088809f7c7f502fca9d9d9214e5e830be52
Alina-Igorevna <Chuveva92@yandex.ru>
2020-03-02 18:47:17 +0200

    add task cssmin

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require ("gulp");
 const browserSync = require('browser-sync').create();
+const cssmin = require('gulp-cssmin');
+const rename = require('gulp-rename');
 
 // Static server
 gulp.task('browser-sync', function() {
@@ -9,4 +11,13 @@ gulp.task('browser-sync', function() {
       }
   });
   gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+// css.min
+gulp.task('mincss', function (done) {
+    gulp.src('./css/*.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('css'));
+    done ();
 });
