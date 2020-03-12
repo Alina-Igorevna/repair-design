db5a54cc84a56ae0a3b662522a3ab3e5904f5e2c
Alina-Igorevna <Chuveva92@yandex.ru>
2020-03-12 17:22:11 +0200

    feat: js function for modal

---
+++ js/main.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+  const modal = document.querySelector('.modal');
+  const madalBtn = document.querySelectorAll('[data-toggle=modal]');
+  const closeBtn = document.querySelector('.modal__close');
+  const switchModal = () => {
+    modal.classList.toggle('modal--visible');
+  }
+
+  madalBtn.forEach(element => {
+    element.addEventListener('click', switchModal);
+  });
+
+  closeBtn.addEventListener('click', switchModal);
+
+});
