d883ee2f479f91b887a6f3795343ec96370d26c7
Alina-Igorevna <Chuveva92@yandex.ru>
2020-03-13 21:11:14 +0200

    add close modal in main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,37 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
+document.addEventListener("DOMContentLoaded", (event) =>{ 
   const modal = document.querySelector('.modal');
-  const madalBtn = document.querySelectorAll('[data-toggle=modal]');
+  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
   const closeBtn = document.querySelector('.modal__close');
+ // Функция события нажатия кнопки
+  const pressESC = (event) => {
+    if(event.keyCode === 27){ // Если код кнопки 27(ESC) закрываем модальную форму
+      document.removeEventListener('keyup', pressESC);
+      modal.classList.remove('modal--visible');
+    }
+  };
+ // Функция открытия и закрытия модального окна 
   const switchModal = () => {
     modal.classList.toggle('modal--visible');
-  }
+    // если modal Имеет класс modal--visible добавляем событие keyup
+    if(modal.classList.contains('modal--visible')){
+      document.addEventListener('keyup', pressESC);
+    }
+    else{ // иначе удаляем событие
+      document.removeEventListener('keyup', pressESC);
+    }
+  };
 
-  madalBtn.forEach(element => {
+  modalBtn.forEach(element => {
     element.addEventListener('click', switchModal);
   });
 
   closeBtn.addEventListener('click', switchModal);
-
+// закрытие модального окна при нажатии вне формы
+  modal.addEventListener('click', (event)=>{
+    const target = event.target;
+    if(target.classList.contains('modal')){
+      switchModal();
+    }
+  });
+  
 });
