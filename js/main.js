3b53e09bce82a9d08688c6fbde2ef881238b6e1b
Alina-Igorevna <Chuveva92@yandex.ru>
2020-03-24 13:35:47 +0200

    edit

--- js/main.js
+++ js/main.js
@@ -422,7 +422,21 @@ $(window).scroll(function() {
   }
 });
 
-
+//ВИДЕО С ЮТУБА
+  var player;
+  $('.video__play__wrap').on('click', function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+      // height: '434',
+      width: '100%',
+      videoId: 'RHzzLqJWqHs',
+      events: {
+        'onReady': videoPlay,
+      }
+    });
+  });
+  function videoPlay(event) {
+    event.target.playVideo();
+  };
 //КОНЕЦ JS
 });
 
